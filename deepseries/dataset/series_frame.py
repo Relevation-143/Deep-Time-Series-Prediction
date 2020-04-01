66e5c8cb5f3d17dd51de81928fd1663796bef19f
unknown <evilp@DESKTOP-TS9S051.chinawyny.com>
2020-04-01 17:30:31 +0800

    'debug'

--- deepseries/dataset/series_frame.py
+++ deepseries/dataset/series_frame.py
@@ -4,6 +4,7 @@
 @contact: evilpsycho42@gmail.com
 @time   : 2020/4/1 16:28
 """
+from torch.utils.data import DataLoader
 
 
 class SeriesFeature:
@@ -15,3 +16,9 @@ class SeriesFrame(object):
     def __init__(self, raw_x, raw_x_valid=None):
         self.raw_x
 
+
+class Data:
+
+    def __iter__(self):
+        for i in range(5):
+            yield i
